fee9d71d744bcca41e3607400fdf197548c80548
Rahmane OUSMANE <ousmanedev@gmail.com>
2017-02-20 00:09:06 +0800

    Ask for version in prompt

--- app/index.js
+++ app/index.js
@@ -3,19 +3,23 @@ var Generator = require('yeoman-generator');
 var download = require('download');
 
 module.exports = Generator.extend({
-  constructor: function (args, opts) {
-    Generator.apply(this, arguments);
-    this.argument('version', { desc: 'Magento 2 version to install',
-                               type: String,
-                               default: '2.1.4' });
+  prompting: function() {
+    return this.prompt([{
+      type    : 'input',
+      name    : 'magento2Version',
+      message : 'Version: ',
+      default : '2.1.4'
+    }]).then((answers) => {
+      this.magento2Version = answers.magento2Version;
+    });
   },
 
   writing: function() {
-    this.log('Started downloading Magento2 version '+ this.options.version +'...');
-    download('https://github.com/magento/magento2/archive/' + this.options.version + '.zip',
+    this.log('Downloading Magento2 version '+ this.magento2Version +'...');
+    download('https://github.com/magento/magento2/archive/' + this.magento2Version + '.zip',
              this.destinationPath(),
              { extract: true }).then(() => {
-              var magentoFolderName = 'magento2-' + this.options.version;
+              var magentoFolderName = 'magento2-' + this.magento2Version;
               this.log('Magento 2 installed in ' + this.destinationPath(magentoFolderName) + '\n\
 Use `cd '+ magentoFolderName + '` to move into your magento2 folder and run `composer install` to install the magento 2 dependencies.');
             });
